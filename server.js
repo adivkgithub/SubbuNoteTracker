dfbf4799523ea7550e04afb29c13a3b4327a56cb
unknown <stalari@ATXWS-5161.NA.ATXGLOBAL.COM>
2016-12-27 15:43:21 -0600

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ var app      = express(); 								// create our app w/ express
 var port  	 = process.env.PORT || 2080; 				// set the port
 var morgan = require('morgan'); 		// log requests to the console (express4)
 var bodyParser = require('body-parser'); 	// pull information from HTML POST (express4)
+
+app.use(function(req, res, next){
+     if(req.headers['x-forwarded-proto'] === 'http'){
+        next();
+     } else {
+        res.redirect('http://' + req.hostname + req.url);
+     }
+});
+
 app.use(express.static(__dirname + '/public')); 				// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); 										// log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-www-form-urlencoded
